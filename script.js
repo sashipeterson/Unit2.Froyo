66b2afb6e980c90d6e0728447f85ea382d02f43c
Hisashi Peterson <146689382+sashipeterson@users.noreply.github.com>
2023-11-06 22:27:05 +0900

    Added function

--- script.js
+++ script.js
@@ -4,36 +4,85 @@
 const inputFlavor = prompt(
     "Welcome to Froyo, please place your order.",
     "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
-  );
+);
 
 // Split the string of numbers into an array of strings.
-const flavorsArray = inputFlavor.split(", ");
+// const flavorsArray = inputFlavor.split(", ");
 
-//Build object to count flavors
-const totalFlavors = {
-    vanilla: 0,
-    strawberry: 0,
-    coffee: 0,
-}
+// //Build object to keep track of flavors
+// const totalFlavors = {
+//     vanilla: 0,
+//     strawberry: 0,
+//     coffee: 0,
+// }
 
 // Convert the array of strings into an array of numbers.
-for (let i = 0; i < flavorsArray.length; i++) {
+// for (let i = 0; i < flavorsArray.length; i++) {
 
-    //If the flavor is vanilla add to object
-    if(flavorsArray[i] === 'vanilla'){
-        totalFlavors.vanilla += 1;
+//     //If the flavor is vanilla add to object
+//     if (flavorsArray[i] === 'vanilla') {
+//         totalFlavors.vanilla += 1;
+//     }
+
+//     //If the flavor is strawberry add to object
+//     else if (flavorsArray[i] === 'strawberry') {
+//         totalFlavors.strawberry += 1;
+//     }
+
+//If the flavor is coffee add to object
+//     else if (flavorsArray[i] === 'coffee') {
+//         totalFlavors.coffee += 1;
+//     }
+
+// }
+
+//Display amount of order on a table.
+console.table(countFlavors(inputFlavor));
+
+//Initialize function call it Count Flavors
+function countFlavors(input) {
+
+    // Split the string of numbers into an array of strings.
+    const flavorsArray = input.split(", ");
+
+    //Build object to keep track of flavors
+    const totalFlavors = {
+        vanilla: 0,
+        strawberry: 0,
+        coffee: 0,
     }
 
-    //If the flavor is strawberry add to object
-    else if(flavorsArray[i] === 'strawberry'){
-        totalFlavors.strawberry += 1;
+    //Convert the array of strings into an array of numbers.
+    for (let i = 0; i < flavorsArray.length; i++) {
+
+        // const key = flavorsArray[i];
+
+        //If flavor hasn't shown up yet create a new key
+        // if (key !== totalFlavors) {
+        //     totalFlavors[key] = key.length;
+        // }
+
+        // else if (key === totalFlavors) {
+        //     totalFlavors[key] = key.length;
+        // }
+
+        //If the flavor is vanilla add to object
+        if (flavorsArray[i] === 'vanilla') {
+            totalFlavors.vanilla += 1;
+        }
+
+        //If the flavor is strawberry add to object
+        else if (flavorsArray[i] === 'strawberry') {
+            totalFlavors.strawberry += 1;
+        }
+
+        //If the flavor is coffee add to object
+        else if (flavorsArray[i] === 'coffee') {
+            totalFlavors.coffee += 1;
+        }
+
     }
 
-    //If the flavor is coffee add to object
-    else if(flavorsArray[i] === 'coffee'){
-        totalFlavors.coffee += 1;
-    }
+    return totalFlavors;
 
 }
-
-console.table(totalFlavors);
